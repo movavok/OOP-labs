bd79508a98e8ba2937dad31680d96162907ebc0b
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-09-23 15:47:33 +0300

    Reworked cpp and h files

--- lab1_employee/employee.h
+++ lab1_employee/employee.h
@@ -4,38 +4,30 @@
 
 using namespace std;
 
-// class Employee {
-// private:
-//     string name = "";
-//     int id = 0;
-//     double salary = 0.0;
-// public:
-//     Employee() {}
-//     Employee(const string& name, int id, double salary) : name(name), id(id), salary(salary) {}
+class Employee {
+private:
+    string name = "";
+    int id = 0;
+    double salary = 0.0;
+public:
+    Employee() {}
+    Employee(const string& name, int id, double salary) : name(name), id(id), salary(salary) {}
 
-//     ~Employee() { cout << "employee \"" << name << "\" was deleted\n";}
+    ~Employee() { cout << "employee \"" << name << "\" was deleted\n";}
 
-//     void setName(const string& name) { this->name = name; }
-//     void setId(int id) { this->id = id; }
-//     void setSalary(double salary) { this->salary = salary; }
+    void setName(const string& name) { this->name = name; }
+    void setId(int id) { this->id = id; }
+    void setSalary(double salary) { this->salary = salary; }
 
-//     const string& getName() { return name; }
-//     int getId() { return id; }
-//     double getSalary() { return salary; }
-// };
+    const string& getName() { return name; }
+    int getId() { return id; }
+    double getSalary() { return salary; }
+};
 
-struct Employee {
+struct S_Employee {
     string name = "";
     int id = 0;
     double salary = 0.0;
 };
-
-void setName(Employee &emp, const string& name) { emp.name = name; }
-void setId(Employee &emp, int id) { emp.id = id; }
-void setSalary(Employee &emp, double salary) { emp.salary = salary; }
-
-const string& getName(const Employee &emp) { return emp.name; }
-int getId(const Employee &emp) { return emp.id; }
-double getSalary(const Employee &emp) { return emp.salary; }
 
 #endif // EMPLOYEE_H
--- lab1_employee/main.cpp
+++ lab1_employee/main.cpp
@@ -2,23 +2,67 @@
 
 int main()
 {
+    cout << "-Creating object with default constructor-\n";
+
     Employee emp1;
-    cout << "Employee's name - \"" << getName(emp1)
-         << "\"\nEmployee's id - " << getId(emp1)
-         << "\nEmployee's salary - " << getSalary(emp1) << "\n\n";
+
+    cout << "Employee's name - \"" << emp1.getName()
+         << "\"\nEmployee's id - " << emp1.getId()
+         << "\nEmployee's salary - " << emp1.getSalary() << "\n\n";
+
+    cout << "-Creating object and setting arguments-\n";
 
     Employee emp2;
-    setName(emp2, "Yarik");
-    setId(emp2, 3);
-    setSalary(emp2, 19999.9);
-    cout << "Employee's name - \"" << getName(emp2)
-         << "\"\nEmployee's id - " << getId(emp2)
-         << "\nEmployee's salary - " << getSalary(emp2) << "\n\n";
 
-    Employee emp3 = {"Vova", 4, 60000};
-    cout << "Employee's name - \"" << getName(emp3)
-         << "\"\nEmployee's id - " << getId(emp3)
-         << "\nEmployee's salary - " << getSalary(emp3) << "\n\n";
+    string name;
+    int id;
+    double salary;
+
+    cout << "Enter employee's name: ";
+    cin >> name;
+    cout << "                   id: ";
+    cin >> id;
+    cout << "               salary: ";
+    cin >> salary;
+    cout << endl;
+
+    emp2.setName(name);
+    emp2.setId(id);
+    emp2.setSalary(salary);
+
+    cout << "Employee's name - \"" << emp2.getName()
+         << "\"\nEmployee's id - " << emp2.getId()
+         << "\nEmployee's salary - " << emp2.getSalary() << "\n\n";
+
+    cout << "-Creating object and setting arguments by second constructor-\n";
+
+    cout << "Enter employee's name: ";
+    cin >> name;
+    cout << "                   id: ";
+    cin >> id;
+    cout << "               salary: ";
+    cin >> salary;
+    cout << endl;
+
+    Employee emp3(name, id, salary);
+    cout << "Employee's name - \"" << emp3.getName()
+         << "\"\nEmployee's id - " << emp3.getId()
+         << "\nEmployee's salary - " << emp3.getSalary() << "\n\n";
+
+    cout << "-Creating structure object-\n";
+
+    cout << "Enter employee's name: ";
+    cin >> name;
+    cout << "                   id: ";
+    cin >> id;
+    cout << "               salary: ";
+    cin >> salary;
+    cout << endl;
+
+    S_Employee emp = {name, id, salary};
+    cout << "Employee's name - \"" << emp.name
+         << "\"\nEmployee's id - " << emp.id
+         << "\nEmployee's salary - " << emp.salary << "\n\n";
 
     return 0;
 }
